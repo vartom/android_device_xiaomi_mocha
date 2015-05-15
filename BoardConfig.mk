fe18add704b55ee90d1567baddad91371efafc06
hak86 <hakamelassouad@gmail.com>
2015-05-15 16:08:30 +0200

    mocha: squashed device tree of Xiaomi Mocha for CM-12.1

---
+++ BoardConfig.mk
@@ -0,0 +1,81 @@
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/xiaomi/mocha/BoardConfigVendor.mk
+
+TARGET_ARCH := arm
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := tegra
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a15
+TARGET_ARCH_VARIANT := armv7-a
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_BOOTLOADER_BOARD_NAME := mocha
+
+BOARD_KERNEL_CMDLINE := 
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --dt device/xiaomi/mocha/dt.img
+
+BOARD_USE_CUSTOM_RECOVERY_FONT :=  \"roboto_23x41.h\"
+TARGET_SCREEN_HEIGHT := 1200
+TARGET_SCREEN_WIDTH := 1920
+BRIGHTNESS_SYS_FILE := /sys/class/leds/button-backlight/brightness	  
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_RECOVERY_SWIPE := true
+BOARD_NATIVE_DUALBOOT := true
+BOARD_NATIVE_DUALBOOT_SINGLEDATA := true
+TARGET_RECOVERY_UI_LIB := librecovery_ui_mocha
+BOARD_BOOTIMAGE_PARTITION_SIZE := 20971520
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 20971520
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 671088640
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 13742637056
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
+#BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/backlight/pwm-backlight/brightness\"
+COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/xiaomi/mocha/recovery/recovery_keys.c
+TARGET_RECOVERY_FSTAB := device/xiaomi/mocha/recovery.fstab
+RECOVERY_SDCARD_ON_DATA := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+
+
+TARGET_PREBUILT_KERNEL := device/xiaomi/mocha/kernel
+
+ARD_CUSTOM_RECOVERY_UI := \
+	../../device/xiaomi/mocha/recovery/dualboot.c \
+	../../device/xiaomi/mocha/recovery/recovery_ui.c
+
+# Graphics
+USE_OPENGL_RENDERER := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR ?= device/xiaomi/mocha/bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/xiaomi/mocha/bluetooth/vnd_mocha.txt
+
+# Wifi related defines
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WLAN_DEVICE                := bcmdhd
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_FW_PATH_STA          := "/etc/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/etc/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P          := "/etc/firmware/fw_bcmdhd_p2p.bin"
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
+#WIFI_DRIVER_MODULE_ARG           := "iface_name=wlan0"
+#WIFI_DRIVER_MODULE_NAME          := "bcmdhd"
+
+BOARD_USES_STOCK_POLICY := true
+
+BOARD_HARDWARE_CLASS := device/xiaomi/mocha/cmhw/
