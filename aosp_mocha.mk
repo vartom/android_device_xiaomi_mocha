4a0541abb18226f3c5fa9b7bac91a79aa5bf6f63
Vartom <babanakov_dima@mail.ru>
2015-05-21 18:10:25 +0500

    Update aosp_mocha.mk

--- aosp_mocha.mk
+++ aosp_mocha.mk
@@ -3,7 +3,7 @@
 #PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
 # Inherit from the common Open Source product configuration
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 
 
@@ -13,10 +13,6 @@ PRODUCT_RELEASE_NAME := mocha
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/mocha/mocha.mk)
-
-#PRODUCT_BUILD_PROP_OVERRIDES += \
-#    BUILD_FINGERPRINT="xiaomi/mocha/mocha:4.4.4/KTU84P/4.12.05:user/release-keys" \
-#    PRIVATE_BUILD_DESC="mocha-user 4.4.4 4.12.05 release-keys"
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mocha
