31bc3029c819561380601c7e5595b8fe59d44fb9
Vartom <babanakov_dima@mail.ru>
2015-05-21 17:53:42 +0500

    Update cm_mocha.mk

--- cm_mocha.mk
+++ cm_mocha.mk
@@ -4,3 +4,9 @@ $(call inherit-product,  $(SRC_TARGET_DIR)/product/full_base.mk)
 
 
 $(call inherit-product, device/xiaomi/mocha/mocha.mk)
+
+PRODUCT_PREBUILT_WEBVIEWCHROMIUM := true
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := cm_mocha
+PRODUCT_DEVICE := mocha
