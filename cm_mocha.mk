17c08930d2b8592a626561f02d513b1d3739c271
Vartom <babanakov_dima@mail.ru>
2015-05-21 17:51:22 +0500

    Update cm_mocha.mk

--- cm_mocha.mk
+++ cm_mocha.mk
@@ -1,2 +1,6 @@
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product,  $(SRC_TARGET_DIR)/product/full_base.mk)
+
+
 $(call inherit-product, device/xiaomi/mocha/mocha.mk)
